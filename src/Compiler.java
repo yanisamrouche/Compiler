f9f63ff8ca40d8a79c80a2941b6cb8457ab0c2c7
Alexis Nasr <alexis.nasr@lif.univ-mrs.fr>
2021-01-29 16:09:30 +0100

    ajout des fichiers java

---
+++ src/Compiler.java
@@ -0,0 +1,186 @@
+import sc.parser.*;
+import sc.lexer.*;
+import sc.node.*;
+import java.io.*;
+import sa.*;
+import ts.*;
+import c3a.*;
+import nasm.*;
+import fg.*;
+import ig.*;
+
+
+public class Compiler
+{
+    public static void main(String[] args)
+    {
+     boolean outputArm = false;
+     boolean outputX86 = true;
+     boolean outputPython = false;
+     PushbackReader br = null;
+     String baseName = null;
+     String inputFileName = null;
+     int verboseLevel = 0;
+	
+	for (int i = 0; i < args.length; i++) {
+	    if(args[i].equals("-v")){
+		    i++;
+		    if (args[i].equals("0")){
+			verboseLevel = 0;
+		    }
+		    if (args[i].equals("1")){
+			verboseLevel = 1;
+		    }
+		    if (args[i].equals("2")){
+			verboseLevel = 2;
+		    }
+	    }
+			
+	    else if(args[i].equals("-f")){
+		    i++;
+		    if (args[i].equals("x86")){
+		    }
+		    else if (args[i].equals("arm")){
+			outputArm = true;
+			outputX86 = false;
+		    }
+		    else if (args[i].equals("python")){
+			outputPython = true;
+			outputX86 = false;
+		    }
+		    else{
+			System.out.println("output format file unknown");
+			System.exit(1);
+		    }
+		}
+	    else{
+		inputFileName = args[i];
+		//		System.out.println(inputFileName);
+	    }
+	}
+	
+	if(inputFileName == null){
+	    System.out.println("specify input file");
+	    System.exit(1);
+	}
+	
+	try {
+	    br = new PushbackReader(new FileReader(inputFileName));
+	    baseName = removeSuffix(inputFileName, ".l");
+	}
+	catch (IOException e) {
+	    e.printStackTrace();
+	} 
+	try {
+	    // Create a Parser instance.
+	    Parser p = new Parser(new Lexer(br));
+	    // Parse the input.
+	    System.out.println("[BUILD SC] ");
+	    Start tree = p.parse();
+	    
+	    if(verboseLevel > 1){
+		System.out.println("[PRINT SC]");
+		tree.apply(new Sc2Xml(baseName));
+	    }
+	    
+	    System.out.println("[BUILD SA] ");
+	    Sc2sa sc2sa = new Sc2sa();
+	    tree.apply(sc2sa);
+	    SaNode saRoot = sc2sa.getRoot();
+
+	    if(verboseLevel > 1){
+		System.out.println("[PRINT SA]");
+		new Sa2Xml(saRoot, baseName);
+	    }
+	    
+	    System.out.println("[BUILD TS] ");
+	    Ts tableGlobale = new Sa2ts(saRoot).getTableGlobale();
+
+	    if(verboseLevel > 1){
+		System.out.println("[PRINT TS]");
+		tableGlobale.afficheTout(baseName);
+	    }
+	    
+	    if(verboseLevel > 0){
+		System.out.println("[EXEC SA] ");
+		SaEval saEval = new SaEval(saRoot, tableGlobale);
+		if(verboseLevel > 1){
+		    System.out.println("[PRINT SA]");
+		    saEval.affiche(baseName);
+		}
+	    }
+	    
+	    System.out.println("[BUILD C3A] ");
+	    C3a c3a = new Sa2c3a(saRoot, tableGlobale).getC3a();
+
+	    if(verboseLevel > 1){
+		System.out.println("[PRINT C3A] ");
+		c3a.affiche(baseName);
+	    }
+	    
+	    if(verboseLevel > 0){
+		System.out.println("[EXEC C3A]");
+		C3aEval c3aEval = new C3aEval(c3a, tableGlobale);
+		c3aEval.affiche(baseName);
+	    }
+	    
+	    if(outputX86){
+		System.out.println("[BUILD PRE NASM] ");
+		Nasm nasm = new C3a2nasm(c3a, tableGlobale).getNasm();
+		if(verboseLevel > 1){
+		    
+		System.out.println("[PRINT PRE NASM] ");
+		nasm.affichePre(baseName);
+		}
+		
+		System.out.println("[BUILD FG] ");
+		Fg fg = new Fg(nasm);
+		
+		if(verboseLevel > 1){
+		    System.out.println("[PRINT FG] ");
+		    fg.affiche(baseName);
+		}
+		
+		System.out.println("[SOLVE FG]");
+		FgSolution fgSolution = new FgSolution(nasm, fg);
+		if(verboseLevel > 1){
+		    System.out.println("[PRINT FG SOLUTION] ");
+		    fgSolution.affiche(baseName);
+		}	    
+		System.out.println("[BUILD IG] ");
+		Ig ig = new Ig(fgSolution);
+		
+		if(verboseLevel > 1){
+		    System.out.println("[PRINT IG] ");
+		    ig.affiche(baseName);
+		}
+
+		System.out.println("[ALLOCATE REGISTERS]");
+		ig.allocateRegisters();
+		
+		System.out.println("[PRINT NASM]");
+		nasm.affiche(baseName);
+	    }
+	    if(outputArm){
+		System.out.println("ARM output not implemented yet");
+	    }
+	    if(outputPython){
+		System.out.println("Python output not implemented yet");
+	    }
+	}
+	catch(Exception e){
+	    System.out.println(e.getMessage());
+	    System.exit(1);
+	}
+    }
+
+
+    public static String removeSuffix(final String s, final String suffix)
+    {
+	if (s != null && suffix != null && s.endsWith(suffix)){
+	    return s.substring(0, s.length() - suffix.length());
+	}
+	return s;
+    }
+    
+}
---
+++ src/Sc2Xml.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import sc.analysis.*;
+import sc.node.*;
+
+class Sc2Xml extends DepthFirstAdapter
+{
+    private int indentation;
+    private String baseFileName;
+    private String fileName;
+    private PrintStream out;
+    
+    public Sc2Xml(String baseFileName)
+    {
+
+	if (baseFileName == null){
+	    this.out = System.out;
+	    
+	}
+	else{
+	    try {
+		this.baseFileName = baseFileName;
+		this.fileName = baseFileName + ".sc";
+		this.out = new PrintStream(this.fileName);
+	    }
+	    
+	    catch (IOException e) {
+		System.err.println("Error: " + e.getMessage());
+	    }
+	}
+    }
+    
+    
+    public void defaultIn(@SuppressWarnings("unused") Node node)
+    {
+	for(int i = 0; i < this.indentation; i++){this.out.print(" ");}
+	this.indentation++;
+	this.out.println("<" + node.getClass().getSimpleName() + ">");
+    }
+
+    public void defaultOut(@SuppressWarnings("unused") Node node)
+    {
+	this.indentation--;
+	for(int i = 0; i < this.indentation; i++){this.out.print(" ");}
+	this.out.println("</" + node.getClass().getSimpleName() + ">");
+    }
+
+}
